4995d821e509d4ffd48d969cd72da02d13c4d4fd
belor-78 <username@example.com>
2019-12-11 11:06:29 +0200

    dshsr

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo rm / etc / nginx / sites-enabled / default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
