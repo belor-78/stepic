639b63f4c1f5c178ceb229031f052c88bccee467
belor-78 <username@example.com>
2019-12-16 19:55:21 +0200

    nvjhbk,ghj

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn --bind='0.0.0.0:8080' hello:application
+sudo gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:application 
 
