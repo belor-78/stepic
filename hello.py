fd13c3b8a2e13dd4ee257fa0d7d033f5ea066838
belor-78 <username@example.com>
2019-12-16 19:27:01 +0200

    agfegr

--- hello.py
+++ hello.py
@@ -1,17 +1,9 @@
-from cgi import parse_qs
-
 def application(environ, start_response):
-
-    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-    body = []
-    for k,v in query.items():
-        for i in v:
-            body.append(k +'=' + i +'/r/n/')
-
     status = '200 OK'
     headers = [
         ('Content-Type','text/plain')
         ]
     start_response(status, headers)
-    return body
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
         
