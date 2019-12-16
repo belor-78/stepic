373df7ab98f62ced0cc9c422f54120358b04dae7
belor-78 <username@example.com>
2019-12-16 21:00:45 +0200

    lkjzfhbv

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-sudo ln -sf etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart -D
-sudo gunicorn -c etc/config.py hello:app -D
-
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
