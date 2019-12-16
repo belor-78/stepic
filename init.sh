bb4cf39ca2945231a52baacdacec797859769051
belor-78 <username@example.com>
2019-12-16 19:44:16 +0200

    hjgikljg

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf etc/gunicorn.d/hello.py
+sudo gunicorn --bind='0.0.0.0:8080' hello:application
 sudo /etc/init.d/gunicorn restart
