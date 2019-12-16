bd2c0e954ab5437087a5e3c7589c47602408fa8b
belor-78 <username@example.com>
2019-12-16 19:50:31 +0200

    kfhykui

--- etc/hello.py
+++
@@ -1,9 +0,0 @@
-def application(environ, start_response):
-    status = '200 OK'
-    headers = [
-        ('Content-Type','text/plain')
-        ]
-    start_response(status, headers)
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
-        
