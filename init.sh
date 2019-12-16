3bb6e01a3354c8f276f243cc9e2dbfe3cb070913
belor-78 <username@example.com>
2019-12-16 19:47:29 +0200

    dfjh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo gunicorn --bind='0.0.0.0:8080' hello:application
-sudo /etc/init.d/gunicorn restart
+
