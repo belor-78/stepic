6d8cfc00660e680059de7d920da14e8b49d368cd
belor-78 <username@example.com>
2019-12-17 23:11:48 +0200

    5

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
+sudo apt update                     # готовимся скачивать питон
+sudo apt install python3.5          # скачиваем питон
+sudo pip3 install django==2.1
+sudo pip3 install gunicorn
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
 sudo cd ask
 sudo cd ask
-sudo cd ask
-sudo gunicorn --bind='0.0.0.0:8000' wsgi:application
+sudo gunicorn -c /home/box/ask/ask/wsgi.py wsgi:application
