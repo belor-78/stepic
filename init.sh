971a6c089a4716f06faf59d178e0181748377681
belor-78 <username@example.com>
2019-12-17 23:34:34 +0200

    9

--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf wsgi:application
+sudo gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application
