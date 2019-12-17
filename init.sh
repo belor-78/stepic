5ef679da2de1a3ee0c55227ca45e8c68d8f02ef2
belor-78 <username@example.com>
2019-12-17 23:26:53 +0200

    8

--- init.sh
+++ init.sh
@@ -8,6 +8,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo cd ask
-sudo cd ask
-sudo gunicorn -c /home/box/web/ask/ask/wsgi.py wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf wsgi:application
