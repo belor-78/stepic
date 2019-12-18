5dff95c0441039dca111fd403d957d511e4e9c01
belor-78 <username@example.com>
2019-12-18 23:27:35 +0200

    1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    title = models.Charfield(max_length=250)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User,on_delete=models.PROTECT)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 sudo apt update                     # готовимся скачивать питон
 sudo apt install python3.5          # скачиваем питон
 sudo rm /usr/bin/python3                         # удалить питон 3.4.3 из системы
@@ -8,4 +11,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 /home/box/web/ask/ask/wsgi:application
+cd /home/box/web/ask
+gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
