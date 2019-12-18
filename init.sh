0cadee45fb9e65b18473e21e4a3ff012a17738e3
belor-78 <username@example.com>
2019-12-18 23:33:47 +0200

    2

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ sudo rm /usr/bin/python3                         # удалить питон 3.4
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3   # поставить на его место питон 3.5
 sudo pip3 install django==2.1
 sudo pip3 install gunicorn
+sudo pip3 install mysqlclient
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
