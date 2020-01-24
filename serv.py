098457a87611e5aae44609ff4bf9dd0653807dea
belor-78 <58696018+belor-78@users.noreply.github.com>
2020-01-24 13:23:35 +0200

    Create serv.py

---
+++ serv.py
@@ -0,0 +1,19 @@
+from socket import *
+
+sock = socket(AF_INET,SOCK_STREAM)
+sock.bind(('0.0.0.0', 2222))
+sock.listen(10)
+
+while True:
+    connection, address = sock.accept()
+    print('connected', address)
+    while True:
+        data = connection.recv(1024)
+        if data:
+            print(data)
+        if data.decode() == 'close':
+            break
+        if not data:
+            break
+        connection.send(data)
+    connection.close()
