4b3f22eea730e54afe39eb6b5af46377a10c45cd
belor-78 <belor-78@rambler.ru>
2020-02-02 10:18:56 +0200

    запускаум wsgi

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo /etc/init.d/nginx stop
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/test.conf
-gunicorn -b 0.0.0.0:8080 hello
+gunicorn -b 0.0.0.0:8080 hello:application
 sudo /etc/init.d/nginx start
