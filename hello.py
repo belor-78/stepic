025adabab674784b4eb0d1ce0ed4b3735e2fd7a9
belor-78 <belor-78@rambler.ru>
2020-02-06 14:24:51 +0200

    one more time

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ def application(env, start_response):
     print(env)
     l = env['QUERY_STRING'].split('&')
     l = [(i+'\n').encode() for i in l]
-    start_responce('200 OK', [('Content-Type', 'text/html')])
+    start_response('200 OK', [('Content-Type', 'text/plain')])
     return l
