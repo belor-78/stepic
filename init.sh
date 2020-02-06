96d28009c53721a62347170c790364e142c2c741
belor-78 <belor-78@rambler.ru>
2020-02-06 13:21:17 +0200

    one more time

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo /etc/init.d/nginx stop
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#gunicorn -b 0.0.0.0:8080 hello:application
+sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+gunicorn -b 0.0.0.0:8080 hello:application
 sudo /etc/init.d/nginx start
