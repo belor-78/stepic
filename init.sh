d6f85aaa8c86562e53b2e1ce96dbadcd35cf6cd4
belor-78 <belor-78@rambler.ru>
2020-02-07 19:54:19 +0200

    second django day

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo /etc/init.d/nginx stop
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #gunicorn -b '0.0.0.0:8080' hello:application
 #sudo gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application  &
