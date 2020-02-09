daa6ee68ff753843f38e47562e9b3c617b17e7c2
belor-78 <belor-78@rambler.ru>
2020-02-09 22:46:52 +0200

    django original

--- init.sh
+++ init.sh
@@ -5,11 +5,11 @@ sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 gunicorn -b '0.0.0.0:8080' hello:application
 #sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application  &
 sudo /etc/init.d/nginx start
-cd ask
-gunicorn -b 0.0.0.0:8000 ask.wsgi
+#cd ask
+#gunicorn -b 0.0.0.0:8000 ask.wsgi
 
-sudo /etc/init.d/mysql start
-sudo -uroot -e 'create database stepik;'
-sudo -uroot -e "create user 'box'@'localhost';"
-sudo -uroot -e "grant all privileges on stepik.* to box@localhost;"
+#sudo /etc/init.d/mysql start
+
+#sudo -uroot -e "create user 'box'@'localhost';"
+#sudo -uroot -e "grant all privileges on stepik.* to 'box'@'localhost';"
 
